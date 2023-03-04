79e60ab2cf8523e9dd39b380c757f7cac13ddd4d
amine <amine.m.a1998@gmail.com>
2023-03-04 09:54:34 +0100

    add car model

--- models/car.js
+++ models/car.js
@@ -3,6 +3,10 @@ const { Schema } = mongoose;
 
 const carSchema = new Schema({
     name : String,
+    description : String,
+    images : [String],
+    isAvailable : Boolean,
+    reservedDates : [{ type: Date, default: Date.now },]
    }) 
 
 const car=mongoose.model('car',carSchema)
